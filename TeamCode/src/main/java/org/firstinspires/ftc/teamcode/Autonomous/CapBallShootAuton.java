c7eaed12ce6e14ad62f66a355fe150ee56577afe
dina.brustein <dina.brustein@livingston.org>
2017-03-11 17:43:45 -0500

    Auton paths

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/CapBallShootAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/CapBallShootAuton.java
@@ -13,8 +13,17 @@ public class CapBallShootAuton extends LinearOpMode {
     Hardware3415 balin = new Hardware3415();
     public void runOpMode(){
         balin.init(hardwareMap, true);
-
         balin.shoot(1);
+        balin.door.setPosition(balin.DOOR_OPEN);
+        balin.collector.setPower(1.0);
+        sleep(2000);
+        balin.collector.setPower(0);
+        balin.door.setPosition(balin.DOOR_CLOSED);
+        sleep(500);
+        //Shoot second particle
+        balin.shoot(1.0);
+        sleep(600);
+        balin.shoot(0);
         sleep(500);
         sleep(10000);
         balin.setDrivePower(.3);
