182c436b4fa45fde8249474eee6d000a936d44b9
sporky1414 <sporky1414@gmail.com>
2017-04-03 17:02:53 -0400

    Begin PixyI2C Code Writing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClasses/PixyCameraI2C.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.TestClasses;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.hardware.I2cDevice;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
+
+/**
+ * Created by andrew.keenan on 4/3/2017.
+ */
+
+public class PixyCameraI2C extends LinearOpMode {
+    public I2cDevice PIXY = null;
+    public I2cDeviceSynch PIXYReader = null;
+    byte[] PIXYCache;
+    public static final int PIXY_REG_START = 0x04;
+    public static final int PIXY_READ_LENGTH = 2;
+
+    public void runOpMode() {
+        PIXY = hardwareMap.i2cDevice.get("pixy");
+        PIXYReader = new I2cDeviceSynchImpl(PIXY, I2cAddr.create8bit(0x98), false);
+        PIXYReader.engage();
+        waitForStart();
+        while(opModeIsActive() && !isStopRequested()) {
+            PIXYCache = PIXYReader.read(PIXY_REG_START, PIXY_READ_LENGTH);
+            double pixyData = PIXYCache[0] & 0xFF;
+            telemetry.addData("Pixy Data", pixyData);
+        }
+    }
+}
